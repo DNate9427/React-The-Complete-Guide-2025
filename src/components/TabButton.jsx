be02803a2916f01f105dc1ecd7b2b091b6d511c0
Dustin <dustinnation@gmail.com>
2025-03-06 22:16:11 -0500

    Managing State and Using Hooks

--- src/components/TabButton.jsx
+++ src/components/TabButton.jsx
@@ -8,6 +8,7 @@
 
 // Destructured approach:
 export default function TabButton({ children, onSelect }) {
+    console.log('TAB COMPONENT EXECUTING');
     return (
         <li>
             <button onClick={onSelect}>{children}</button>
