b6217d7782af0ace070463f6f69b26664a098399
Dustin <dustinnation@gmail.com>
2025-03-11 22:56:15 -0400

    Dynamic Styling Example

--- src/components/TabButton.jsx
+++ src/components/TabButton.jsx
@@ -7,10 +7,10 @@
 // }
 
 // Destructured approach:
-export default function TabButton({ children, onSelect }) {
+export default function TabButton({ children, onSelect, isSelected }) {
     console.log('TAB COMPONENT EXECUTING');
     return (
         <li>
-            <button onClick={onSelect}>{children}</button>
+            <button className={isSelected ? 'active' : undefined } onClick={onSelect}>{children}</button>
         </li>);
 }
