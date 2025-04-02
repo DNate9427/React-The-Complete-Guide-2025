3c119ac15ccc9e967972a0fc74b29bcd179f7922
Dustin <dustinnation@gmail.com>
2025-04-02 01:36:53 -0400

    GameBoard.jsx - Updating Object State Ummutably

--- 07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
+++ 07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const initialGameBoard = [
     [null, null, null],
     [null, null, null],
@@ -5,14 +7,31 @@ const initialGameBoard = [
 ];
 
 export default function GameBoard() {
+    const [gameBoard, setGameBoard] = useState(initialGameBoard);
+    
+    // Update Object-State Immutability
+    // Objects and arrays (which are objects, technically) are reference values.
+    // You should NOT mutate them directly - instead create a (deep) copy first
+
+    // This would update the old value in memory immediately - before the schedule state update is executed by React:
+    // previousGameBoard[rowIndex][columnIndex] = 'X';
+    function handlePlayerMove(rowIndex, columnIndex) {
+        setGameBoard((previousGameBoard) => {
+            const updatedBoard = [...previousGameBoard.map(innerArray => [...innerArray])];
+
+            updatedBoard[rowIndex][columnIndex] = 'X';
+            return updatedBoard;
+        });
+    }
+
     return (
         <ol id="game-board">
-            {initialGameBoard.map((row, rowIndex) => (
+            {gameBoard.map((row, rowIndex) => (
                 <li key={rowIndex}>
                     <ol>
                         {row.map((playerSymbol, columnIndex) => (
                             <li key={columnIndex}>
-                                <button>{playerSymbol}</button>
+                                <button onClick={() => handlePlayerMove(rowIndex, columnIndex)}>{playerSymbol}</button>
                             </li>
                         ))}
                     </ol>
