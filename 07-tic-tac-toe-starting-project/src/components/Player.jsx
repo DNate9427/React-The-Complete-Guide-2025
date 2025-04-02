784cf8f9981eeddb92ba15a7b9c3aac48ad4aa45
Dustin <dustinnation@gmail.com>
2025-04-02 00:43:58 -0400

    Updating State Based On Old State Correctly

--- 07-tic-tac-toe-starting-project/src/components/Player.jsx
+++ 07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -5,7 +5,16 @@ export default function Player({ name, symbol }) {
     
 
     function handleEditClick() {
-        setIsEditing(!isEditing);
+        // If your new state depends on your previous state value,
+        // you should NOT update the state with the prior state's value
+
+        // Instead, pass a function to your state updating function.
+        // This function will automatically be called by React and 
+        // will receive the guaranteed latest state value.
+
+        // State update are NOT performed instantly, but at some point in the future (when React has time for it)
+        // In most cases, those state updates are still executed almost instantly.
+        setIsEditing((editing) => !editing);
     }
 
     let buttonText = "Edit";
@@ -16,7 +25,7 @@ export default function Player({ name, symbol }) {
         : buttonText = "Edit";
 
     isEditing
-        ? playerNameRow = <input type="text" required />
+        ? playerNameRow = <input type="text" required value={name}/>
         : playerNameRow = <span className="player-name">{name}</span>;
 
     return (
